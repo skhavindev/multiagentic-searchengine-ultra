2e2cb327bd55a9227a14d316de84e319b672cbec
KHAVIN S <147326178+skhavindev@users.noreply.github.com>
2024-10-02 15:35:20 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -129,6 +129,9 @@ def main():
         visibility: hidden;
         display: none;
         }
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
 
         .stApp {
             font-family: 'DM Serif Display', serif;
